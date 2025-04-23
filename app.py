9037be565114d60140d5a93dd9a32bc648d4d02c
datauser555 <data_user@talonlogisticsinc.com>
2025-04-23 18:28:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     name = request.args.get('name', 'World')
-    return f"Hello, {name}!"
+    return f"Hello,this is a test {name}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
