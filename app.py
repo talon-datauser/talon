01e8b1b08d1ecb51d89f63e927e2455309151644
datauser555 <data_user@talonlogisticsinc.com>
2025-04-24 10:31:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    name = request.args.get('name', 'World')
-    return f"Hello, {name}!"
+    return "Hello from Cloud Run!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
